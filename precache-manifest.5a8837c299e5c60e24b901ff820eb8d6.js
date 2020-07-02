84be8af2dd6bc5d63d919e401c478086defda04a
ueno10 <uenomizuki.nu@gmail.com>
2020-07-03 01:11:30 +0900

    Updates

---
+++ precache-manifest.5a8837c299e5c60e24b901ff820eb8d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ba3305051067fdc0d8bf79e5267f61",
+    "url": "/word-cloud/index.html"
+  },
+  {
+    "revision": "979d597495a8410f5ee3",
+    "url": "/word-cloud/static/js/2.d5d552c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/word-cloud/static/js/2.d5d552c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed800cc4955084d1dd5",
+    "url": "/word-cloud/static/js/main.ead1d12c.chunk.js"
+  },
+  {
+    "revision": "31ef349d0d53f4565ce4",
+    "url": "/word-cloud/static/js/runtime-main.d32e8ca0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/word-cloud/precache-manifest.5a8837c299e5c60e24b901ff820eb8d6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/word-cloud/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
